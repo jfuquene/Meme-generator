0f0991519dabfde0bdf2f8c31cc08748fdd75880
John Fuquene <jfuquene@email.wm.edu>
2021-09-07 17:52:46 -0400

    two new files

---
+++ meme-generator/src/MemeGenerator.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react"
+
+class MemeGenerator extends Component {
+    constructor(){
+        super()
+        this.state = {}
+    }
+
+        render(){
+            return (
+                <h1>Meme MemeGenerator</h1>
+            )
+        }
+    }
+
+
+export default MemeGenerator
